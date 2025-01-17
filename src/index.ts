4c8376bfc83c5e63e3a06b9472269ab109f3879a
NullPointError07 <md.taukir.alam@g.bracu.ac.bd>
2025-01-17 19:41:35 +0600

    wip: google-oauth2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import passport from "passport";
+import session from "express-session";
 import textRoutes from "./routes/textRoutes";
 
 const app = express();
+
 app.use(express.json());
+app.use(session({ secret: process.env.SECRET || "", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const port = process.env.PORT || 3000;
 
